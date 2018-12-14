412e3e390579340b165ac0c3e4221856fa47af25
Jacaplaca <dziewanowski@gmail.com>
2018-12-14 13:51:40 +0100

    redux error devtools-extension installed for client

--- client/src/store.js
+++ client/src/store.js
@@ -1,13 +1,31 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-import rootReducer from './reducers';
+// import { createStore, applyMiddleware, compose } from 'redux';
+// import thunk from 'redux-thunk';
+// import rootReducer from './reducers';
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import reduxThunk from "redux-thunk";
 
-const inititalState = {};
+import reducers from "./reducers";
 
+// const inititalState = {};
+
+// const store = createStore(
+//         rootReducer,
+//         inititalState,
+//         compose(applyMiddleware(thunk),
+//                 window.__REDUX_DEVTOOLS_EXTENSION__&& window.__REDUX_DEVTOOLS_EXTENSION__()));
+
+// export default store;
+
+// const store = createStore(reducers, {}, composeWithDevTools((applyMiddleware(reduxThunk)));
 const store = createStore(
-        rootReducer, 
-        inititalState, 
-        compose(applyMiddleware(thunk), 
-                window.__REDUX_DEVTOOLS_EXTENSION__&& window.__REDUX_DEVTOOLS_EXTENSION__()));
+  reducers,
+  composeWithDevTools(
+    applyMiddleware(reduxThunk)
+    // other store enhancers if any
+  )
+);
+
+// const store = createStore(reducers, applyMiddleware(reduxThunk));
 
 export default store;
